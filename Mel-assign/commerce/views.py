6a493a70b20c4b72558c259e985c2f4453ee35ce
melatbel <151053766+melatbel@users.noreply.github.com>
2024-02-29 14:18:43 +0300

    Update views.py

--- Mel-assign/commerce/views.py
+++ Mel-assign/commerce/views.py
@@ -17,9 +17,3 @@ def about(requests):
 
 def contacts(requests):
     return render(requests, 'contact.html')
-def signup_view(request):
-    signup_text = "Sign up for our website"
-    return render(request, 'signup.html', {'signup_text': signup_text})
-def product(request):
-    product_title = "This is the news page"
-    product_content = "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents."
